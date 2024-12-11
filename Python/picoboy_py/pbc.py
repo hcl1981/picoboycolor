4a1dc6f9941d13e109fa57a5f3e3a04b59ac8cba
ChristophOes <christoph.m.oestreicher@gmail.com>
2024-12-11 19:52:01 +0100

    included framebuf into pbc class and added boundary conditions

--- Python/picoboy_py/pbc.py
+++ Python/picoboy_py/pbc.py
@@ -3,6 +3,7 @@ import utime as time
 import random
 from machine import Pin, SPI
 from st7789 import *
+import framebuf
 
 class PBC():
     LED_RED = Pin(5, Pin.OUT)
@@ -19,6 +20,10 @@ class PBC():
     
     SPEAKER = Pin(15, Pin.OUT)
     
+    BUFFER_WIDTH = 240
+    BUFFER_HEIGHT = 280
+    
+
     def __init__(self,baudrate=31250000):
         BACKLIGHT_PIN = 26
         RESET_PIN = 9
@@ -27,13 +32,17 @@ class PBC():
         CLK_PIN = 18
         DIN_PIN = 19
         spi = SPI(0, baudrate, sck=Pin(CLK_PIN), mosi=Pin(DIN_PIN))
-        self.tft = ST7789(spi, 240, 280,  #st7789
+        self.tft = ST7789(spi, self.BUFFER_WIDTH, self.BUFFER_HEIGHT,  #st7789
             reset=Pin(RESET_PIN, Pin.OUT),
             cs=Pin(CS_PIN, Pin.OUT),
             dc=Pin(DC_PIN, Pin.OUT),
             backlight=Pin(BACKLIGHT_PIN, Pin.OUT),
             rotation=2)
         self.tft.init()
+        
+        buffer = bytearray(self.BUFFER_WIDTH * self.BUFFER_HEIGHT * 2)
+        self.canvas = framebuf.FrameBuffer(buffer, self.BUFFER_WIDTH, self.BUFFER_HEIGHT, framebuf.RGB565)
+        
         
     def __getattr__(self, attr):
         if hasattr(self.tft, attr):
@@ -56,8 +65,52 @@ class PBC():
     def pressedCenter(self):
         return self.JOY_CENTER.value()==0
     
+    def pressedA(self):
+        return self.BUTTON_A.value()==0
+    
+    def pressedB(self):
+        return self.BUTTON_B.value()==0
+    
     def delay(self,millis):
         time.sleep_ms(millis)
 
     def reset(self):
-        super().reset(self.res)   
+        super().reset(self.res)
+        
+        
+    def setPeriodicBoundary(self, x, y):
+        
+        if x < 0:
+            x = self.BUFFER_WIDTH + x
+            
+        if x >= self.BUFFER_WIDTH:
+            x = x - self.BUFFER_WIDTH
+            
+        if y < 0:
+            y = self.BUFFER_HEIGHT + y
+            
+        if y >= self.BUFFER_HEIGHT:
+            y  = y - self.BUFFER_HEIGHT
+        
+        return x, y
+    
+    def setHardBoundary(self, x, y, radius=0):
+        
+        if x < (0 + radius):
+            x = 0 + radius
+            
+        if x >= (self.BUFFER_WIDTH - radius):
+            x = self.BUFFER_WIDTH - 1 - radius
+            
+        if y < (0 + radius):
+            y = 0 + radius
+            
+        if y >= (self.BUFFER_HEIGHT - radius):
+            y  = self.BUFFER_HEIGHT - 1 - radius
+        
+        return x, y
+    
+    def draw(self):
+        self.tft.blit_buffer(self.canvas, 0, 0, self.BUFFER_WIDTH, self.BUFFER_HEIGHT)    
+        
+
